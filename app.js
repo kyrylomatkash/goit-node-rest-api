2b66c62e8d65ab657617c9e4e52af849893ea849
Kyrylo <kyrylo.mm@gmail.com>
2024-02-29 12:23:42 +0100

    Completed

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 
+import contactsRoute from "./route/contactsRoute.js";
+
 const app = express();
 
 app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/contacts", contactsRouter);
+app.use("/api/contacts", contactsRoute);
 
 app.use((_, res) => {
   res.status(404).json({ message: "Route not found. Try again" });
